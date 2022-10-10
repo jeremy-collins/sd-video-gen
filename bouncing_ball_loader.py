fix causal masking and training objective, add output logging, fix input scaling bug

bouncing_ball_loader.py
@@ -20,6 +20,10 @@ class BouncingBall(data.Dataset):
         self.stride = stride
         self.sd_utils = SDUtils()
         self.indices, self.dataset = self.get_data(shuffle=shuffle)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = Transformer()       
+        self.SOS_token = torch.ones((1, model.dim_model), dtype=torch.float32, device=device) * 2
+        self.EOS_token = torch.ones((1, model.dim_model), dtype=torch.float32, device=device) * 3
 
     def __getitem__(self, index):
         # obtaining file paths
@@ -43,10 +47,17 @@ class BouncingBall(data.Dataset):
             # frame = frame.permute(2, 0, 1)
             # frame = frame.float()/255.0
             
+            # bs, seq_len, _, _, _ = frame.shape
+            frame = frame.flatten()
+                
             frames.append(frame)
         
         frames = torch.stack(frames, dim=0)
-            
+        
+        # concatenating SOS token, 
+        frames = torch.cat((self.SOS_token, frames), dim=0)
+                    
+        #  frames.shape: (seq_len + 1, dim_model)   
         return self.indices[index], frames
 
     def __len__(self):

positional_encoding.py
@@ -15,7 +15,7 @@ class PositionalEncoding(nn.Module):
         
         # Encoding - From formula
         pos_encoding = torch.zeros(max_len, dim_model)
-        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4, 5
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1) # 0, 1, 2, 3, 4
         division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model) # 1000^(2i/dim_model)
         
         # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
@@ -26,6 +26,8 @@ class PositionalEncoding(nn.Module):
         
         # Saving buffer (same as parameter without gradients needed)
         pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        # pos_encoding = pos_encoding.unsqueeze(0)
+        
         self.register_buffer("pos_encoding",pos_encoding)
         
     def forward(self, token_embedding: torch.tensor) -> torch.tensor:

predict.py
@@ -15,23 +15,40 @@ def predict(model, input_sequence, max_length=5, SOS_token=2, EOS_token=3):
     model.eval()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # y_input = torch.tensor([[SOS_token]], dtype=torch.long, device=device)
-    y_input = torch.tensor(torch.zeros((1,1,256)), dtype=torch.float32, device=device)
-
+    # y_input = torch.ones((1, model.dim_model), dtype=torch.float32, device=device) * 2 # SOS token
+    SOS_token = torch.ones((1, 1, model.dim_model), dtype=torch.float32, device=device) * 2
+    EOS_token = torch.ones((1, 1, model.dim_model), dtype=torch.float32, device=device) * 3
+    # y_input = torch.tensor([SOS_token], dtype=torch.float32, device=device)
+    y_input = SOS_token
+    
     # num_tokens = len(input_sequence[0])
     with torch.no_grad():
-        for _ in range(1):
+        for _ in range(6):
         # for _ in range(max_length):
             # Get source mask
+            print('input_sequence: ', input_sequence.shape)
+            print('y_input: ', y_input.shape)
+            
             tgt_mask = model.get_tgt_mask(y_input.size(1)).to(device)
             
             pred = model(input_sequence, y_input, tgt_mask)
             
+            # Permute pred to have batch size first again
+            pred = pred.permute(1, 0, 2)
+            
             # next_item = pred.topk(1)[1].view(-1)[-1].item() # num with highest probability
             print('pred', pred.shape)
+            
+            # X shape is (batch_size, src sequence length, input.shape)
+            # y_input shape is (batch_size, tgt sequence length, input.shape)
+        
+            # next item is the last item in the predicted sequence
             next_item = pred[:,-1,:].unsqueeze(1)
             # next_item = torch.tensor([[next_item]], device=device)
+            
+            print('next_item: ', next_item.shape)
 
-            # Concatenate previous input with predicted best word
+            # Concatenate previous input with prediction
             y_input = torch.cat((y_input, next_item), dim=1)
 
             # Stop if model predicts end of sentence
@@ -39,12 +56,12 @@ def predict(model, input_sequence, max_length=5, SOS_token=2, EOS_token=3):
             #     break
 
     # return y_input.view(-1).tolist()
-    return pred[0,0].view(-1).tolist()
+    # return pred[0,0].view(-1).tolist()
+    return pred[0, -1]
     
-  
 if __name__ == "__main__":  
     parser = argparse.ArgumentParser()
-    parser.add_argument('--index', type=int, required=True)
+    parser.add_argument('--index', type=str, required=True)
     args = parser.parse_args()
     
     sd_utils = SDUtils()
@@ -59,7 +76,7 @@ if __name__ == "__main__":
     
     with torch.no_grad():
         for index_list, batch in test_loader:
-            X = batch[:, :-1]
+            X = batch
             y = batch
             
             X = torch.tensor(X, dtype=torch.float32, device=device)
@@ -76,12 +93,11 @@ if __name__ == "__main__":
             sequence_length = y_input.size(1)
             tgt_mask = model.get_tgt_mask(sequence_length).to(device)
             
+            # X shape is (batch_size, src sequence length, input.shape)
             result = predict(model, X)
             result = torch.tensor(result, dtype=torch.float32, device=device)
 
             pred_latents = result.reshape((1, 4, 8, 8))
-            # pred_latents = result.reshape((1, 8, 8, 4))
-            # pred_latents = pred_latents.permute(0, 3, 1, 2)
             pred_img = sd_utils.decode_img_latents(pred_latents)
             
             # counting number of files in ./checkpoints
@@ -94,12 +110,12 @@ if __name__ == "__main__":
             # pred_img[0].show()
             
             for idx, input in enumerate(X.squeeze(0)):
+                if idx == 0:
+                    continue # SOS token
                 print('X', X.shape)
                 input_latents = input.reshape((1, 4, 8, 8))
-                # input_latents = input.reshape((1, 8, 8, 4))
-                # input_latents = input_latents.permute(0, 3, 1, 2)
                 input_img = sd_utils.decode_img_latents(input_latents)
-                img_path = os.path.join('./images', str(folder_index), str(index_list[idx].item()) + '_gt.png')
+                img_path = os.path.join('./images', str(folder_index), str(index_list[idx - 1].item()) + '_gt.png')
                 input_img[0].save(img_path)
             
             # print('result', result)

trainer.py
@@ -9,6 +9,9 @@ from torchvision.datasets import UCF101
 from bouncing_ball_loader import BouncingBall
 import argparse
 import os
+from tqdm import tqdm
+from sd_utils import SDUtils
+import cv2
 
 def train_loop(model, opt, loss_fn, dataloader):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -16,10 +19,12 @@ def train_loop(model, opt, loss_fn, dataloader):
     model = model.to(device)
     model.train()
     total_loss = 0
-    for i, (index_list, batch) in enumerate(dataloader):
-        print('batch', i)
-        if i >= 100:
-            break
+    sd_utils = SDUtils()
+        
+    for i, (index_list, batch) in enumerate(tqdm(dataloader)):
+        # if i >= 99:
+        #     break
+        
         # X, y = batch[:, 0], batch[:, 1]
         
         # X = batch[:, :-1]
@@ -28,10 +33,15 @@ def train_loop(model, opt, loss_fn, dataloader):
         X = batch
         y = batch
         
-        X, y = torch.tensor(X).to(device), torch.tensor(y).to(device)
+        X = torch.tensor(X).to(device)
+        y = torch.tensor(y).to(device)
         
-        y_input = y
-        y_expected = y
+        # y_input = y
+        # y_expected = y
+        
+        # shift the tgt by one so we always predict the next embedding
+        y_input = y[:,:-1]
+        y_expected = y[:,1:]
         
         y_expected = y_expected.reshape(y_expected.shape[0], y_expected.shape[1], -1)
         y_expected = y_expected.permute(1, 0, 2)
@@ -39,15 +49,25 @@ def train_loop(model, opt, loss_fn, dataloader):
         # Get mask to mask out the next words
         sequence_length = y_input.size(1)
         tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+       
+        # X shape is (batch_size, src sequence length, input.shape)
+        # y_input shape is (batch_size, tgt sequence length, input.shape)
 
         # Standard training except we pass in y_input and tgt_mask
         pred = model(X, y_input, tgt_mask)
         # pred = None
-
+        
         # Permute pred to have batch size first again
         # pred = pred.permute(1, 2, 0)
         
-        loss = loss_fn(pred, y_expected)
+        # check decoding
+        # gt = y_expected[-1][-1].reshape((1, 4, 8, 8))
+        # gt_reconstruction = sd_utils.decode_img_latents(gt)
+        # gt_reconstruction = np.array(gt_reconstruction[0])
+        # cv2.imshow('gt_reconstruction', gt_reconstruction)
+        # cv2.waitKey(0)
+        
+        loss = loss_fn(pred[-1], y_expected[-1])
 
         opt.zero_grad()
         loss.backward()
@@ -55,30 +75,31 @@ def train_loop(model, opt, loss_fn, dataloader):
     
         total_loss += loss.detach().item()
         
-    # return total_loss / len(dataloader)
-    return total_loss / 100.0
-
+    return total_loss / len(dataloader)
+    # return total_loss / 100.0
 
 def validation_loop(model, loss_fn, dataloader):  
     model.eval()
     total_loss = 0
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     with torch.no_grad():
-        for j, (index_list, batch) in enumerate(dataloader):
-            print('batch', j)
-            if j >= 100:
-                break
+        for j, (index_list, batch) in enumerate(tqdm(dataloader)):
+            # if j >= 99:
+            #     break
         
             # X, y = batch[:, 0], batch[:, 1]
-            X = batch[:, :-1]
+            
+            # X = batch[:, :-1]
+            # y = batch[:,-1].unsqueeze(1)
+            
+            X = batch
             y = batch
             
-            # X, y = torch.tensor(X, dtype=torch.long, device=device), torch.tensor(y, dtype=torch.long, device=device)
-            X, y = torch.tensor(X, dtype=torch.float32, device=device), torch.tensor(y, dtype=torch.float32, device=device) # TODO: check if this is correct
-
-            # Now we shift the tgt by one so with the <SOS> we predict the token at pos 1
-            y_input = y[:,:-1]
-            y_expected = y[:,1:]
+            X = torch.tensor(X).to(device)
+            y = torch.tensor(y).to(device)
+            
+            y_input = y
+            y_expected = y
             
             y_expected = y_expected.reshape(y_expected.shape[0], y_expected.shape[1], -1)
             y_expected = y_expected.permute(1, 0, 2)
@@ -86,13 +107,18 @@ def validation_loop(model, loss_fn, dataloader):
             # Get mask to mask out the next words
             sequence_length = y_input.size(1)
             tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+        
+            # X shape is (batch_size, src sequence length, input.shape)
+            # y_input shape is (batch_size, tgt sequence length, input.shape)
 
-            # Standard training except we pass in y_input and src_mask
+            # Standard training except we pass in y_input and tgt_mask
             pred = model(X, y_input, tgt_mask)
+            # pred = None
 
             # Permute pred to have batch size first again
-            pred = pred.permute(1, 0, 2)
-            loss = loss_fn(pred, y_expected)
+            # pred = pred.permute(1, 2, 0)
+            
+            loss = loss_fn(pred[-1], y_expected[-1])
             total_loss += loss.detach().item()
         
     return total_loss / len(dataloader)
@@ -114,14 +140,13 @@ def fit(model, opt, loss_fn, train_dataloader, val_dataloader, epochs):
         
         print(f"Training loss: {train_loss:.4f}")
         print(f"Validation loss: {validation_loss:.4f}")
-        print()
     
     # counting number of files in ./checkpoints
     index = len(os.listdir('./checkpoints'))    
     
-    # save model
-    torch.save(model.state_dict(), './checkpoints/model' + '_' + str(index) + '.pt')
-    print('model saved as model' + '_' + str(index) + '.pt')
+    # # save model
+    # torch.save(model.state_dict(), './checkpoints/model' + '_' + str(index) + '.pt')
+    # print('model saved as model' + '_' + str(index) + '.pt')
         
     return train_loss_list, validation_loss_list
 
@@ -140,14 +165,14 @@ if __name__ == "__main__":
     # torch.multiprocessing.set_start_method('spawn')
     
     model = Transformer(num_tokens=0, dim_model=256, num_heads=8, num_encoder_layers=6, num_decoder_layers=6, dropout_p=0.1)
-    opt = optim.Adam(model.parameters(), lr=0.0001)
+    opt = optim.Adam(model.parameters(), lr=0.00001)
     loss_fn = nn.MSELoss() # TODO: change this to mse + condition + gradient difference
     
     frames_per_clip = 5
     step_between_clips = 1
     batch_size = 4
     
-    if args.dataset == 'ucf101':    
+    if args.dataset == 'ucf':    
         ucf_data_dir = "/Users/jsikka/Documents/UCF-101"
         ucf_label_dir = "/Users/jsikka/Documents/ucfTrainTestlist"
         
@@ -193,5 +218,13 @@ if __name__ == "__main__":
     #     print(i.size())
     #     print(i)
     #     break
+    
+    # train_loss_list, validation_loss_list = fit(model, opt, loss_fn, train_loader, test_loader, 10)
 
-    train_loss_list, validation_loss_list = fit(model, opt, loss_fn, train_loader, test_loader, 1)
+    best_loss = 1e10
+    while True:
+        train_loss_list, validation_loss_list = fit(model, opt, loss_fn, train_loader, test_loader, 1)
+        if validation_loss_list[-1] < best_loss:
+            best_loss = validation_loss_list[-1]
+            torch.save(model.state_dict(), './checkpoints/model_best.pt')
+            print('model saved as model_best.pt')

transformer.py
@@ -26,7 +26,7 @@ class Transformer(nn.Module):
 
         # LAYERS
         self.positional_encoder = PositionalEncoding(
-            dim_model=dim_model, dropout_p=dropout_p, max_len=5000
+            dim_model=dim_model, dropout_p=dropout_p, max_len=6
         )
         # self.embedding = nn.Embedding(num_tokens, dim_model)
         self.embedding = nn.Linear(self.height * self.width * 4, dim_model)
@@ -41,12 +41,9 @@ class Transformer(nn.Module):
         
     def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
         
-        # Src size must be (batch_size, src sequence length)
-        # Tgt size must be (batch_size, tgt sequence length)
-        bs, seq_len, _, _, _ = src.shape
-        src = src.reshape(bs, seq_len, self.height * self.width * 4)
-        tgt = tgt.reshape(bs, tgt.shape[1], self.height * self.width * 4)
-        
+        # Src size must be (batch_size, src sequence length, dim_model)
+        # Tgt size must be (batch_size, tgt sequence length, dim_model)
+
         # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
         src = self.embedding(src) * math.sqrt(self.dim_model)
         tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
@@ -76,7 +73,6 @@ class Transformer(nn.Module):
         mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
         
         # mask = self.transformer.generate_square_subsequent_mask(1)
-        
         
         # EX for size=5:
         # [[0.,   0.,   0.,   0.,   -inf.],