454a15ebf90506754c3b16ada545e0addf5bb691
Jeremy <jer044@hotmail.com>
2022-11-28 13:41:28 -0500

    11/28 config sync

--- train_hela.sh
+++ train_hela.sh
@@ -1,6 +1,14 @@
-python trainer_text.py --dataset ucf --save_best True --config 11_27_ucf_text_final
-python trainer_text.py --dataset ucf --save_best True --config 11_27_ucf_text_final
-python trainer_text.py --dataset ucf --save_best True --config 11_27_ucf_text_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_contrastive_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_gdl_final
+
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_contrastive_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_gdl_final
+
+# python trainer_text.py --dataset ucf --save_best True --config 11_27_ucf_text_final
+# python trainer_text.py --dataset ucf --save_best True --config 11_27_ucf_text_final
+# python trainer_text.py --dataset ucf --save_best True --config 11_27_ucf_text_final
 
 # python trainer.py --dataset ucf --save_best True --config 11_26_ucf_4enc_8dec
 # python trainer.py --dataset ucf --save_best True --config 11_26_ucf_5fps
--- train_thanos.sh
+++ train_thanos.sh
@@ -1,4 +1,15 @@
-python trainer.py --dataset ucf --save_best True --config 11_27_ucf_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_gdl_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_contrastive_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_final
+
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_gdl_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_contrastive_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_final
+
+# python trainer.py --dataset ucf --save_best True --config 11_27_ucf_final
+# python trainer.py --dataset ucf --save_best True --config 11_27_ucf_final
+# python trainer.py --dataset ucf --save_best True --config 11_27_ucf_final
+
 
 # python trainer.py --dataset ball --save_best True --config 11_26_bouncing_ball_complex --folder data/complex_large_10_22 --resume True --old_name 11_26_bouncing_ball_complex_2_test
 # python trainer.py --dataset ucf --save_best True --config 11_26_ucf_16heads
