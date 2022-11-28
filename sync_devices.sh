74aa90c1ea421f6333410612f5000bd1d4ae818c
Jeremy <jer044@hotmail.com>
2022-11-27 19:56:53 -0500

    sync machines 11/27

--- sync_devices.sh
+++ sync_devices.sh
@@ -1,4 +1,5 @@
 # TARGET_HOSTNAME="jcollins90@hrl-ultron"
 TARGET_HOSTNAME="patrick@143.215.225.230"
 
-rsync -aP ~/sd-video-gen/ $TARGET_HOSTNAME:~/sd-video-gen/
+# rsync -aP ~/sd-video-gen/ $TARGET_HOSTNAME:~/sd-video-gen/
+rsync -aP $TARGET_HOSTNAME:~/sd-video-gen/checkpoints ~/mnt/extdisk/sd-video-gen/checkpoints
