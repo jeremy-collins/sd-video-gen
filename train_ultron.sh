e0f16276366da48d55255a99185a37b7f9e8eea1
Jeremy <jer044@hotmail.com>
2022-11-27 21:53:20 -0500

    11/27 config sync

--- train_ultron.sh
+++ train_ultron.sh
@@ -1,1 +1,7 @@
-python trainer.py --dataset ucf --save_best True --config 11_27_ucf_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_gdl_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_contrastive_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_final
+
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_gdl_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_contrastive_final
+python trainer.py --dataset ucf --save_best True --config 11_27_ucf_mse_final
