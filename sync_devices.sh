70b842a945a25acf985989c4f72c6de3c113e6ef
Jeremy <jer044@hotmail.com>
2022-11-28 05:40:03 -0500

    11/27 sync config

--- sync_devices.sh
+++ sync_devices.sh
@@ -1,5 +1,6 @@
-# TARGET_HOSTNAME="jcollins90@hrl-ultron"
-TARGET_HOSTNAME="patrick@143.215.225.230"
+TARGET_HOSTNAME="jcollins90@hrl-ultron"
+# TARGET_HOSTNAME="patrick@143.215.225.230"
 
 # rsync -aP ~/sd-video-gen/ $TARGET_HOSTNAME:~/sd-video-gen/
-rsync -aP $TARGET_HOSTNAME:~/sd-video-gen/checkpoints/ ~/sd-video-gen/checkpoints/
+# rsync -aP $TARGET_HOSTNAME:~/sd-video-gen/checkpoints/ ~/sd-video-gen/checkpoints/
+rsync -aP ~/sd-video-gen/data/UCF-101/ $TARGET_HOSTNAME:~/sd-video-gen/data/UCF-101/
